6798d5b7726349170305a185c35ffb2dc7de8489
Ivan Krylov <krylov.r00t@gmail.com>
2020-03-11 16:07:24 +0300

    txtimage: rename parameter, T->TRUE, docs improved
     - use TRUE/FALSE keywords instead of T/F globals
     - transpose is a better name for image.transpose
     - document everything I could think of
     - bring back the shade/block example

--- R/txtimage.R
+++ R/txtimage.R
@@ -1,6 +1,6 @@
 # vi:ts=2 et:
 txtimage <- function(
-  z, width, height, yaxis = c('up', 'down'), image.transpose = T, na.char = ' ',
+  z, width, height, yaxis = c('up', 'down'), transpose = TRUE, na.char = ' ',
   alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 ) {
   stopifnot(!is.infinite(z)) # check for +/- Inf before performing any computations involving range()
@@ -9,17 +9,17 @@ txtimage <- function(
   if (length(alphabet) == 1) alphabet <- strsplit(alphabet, NULL)[[1]]
   stopifnot(nchar(alphabet) == 1)
 
-  if (image.transpose) z <- t(z)
+  if (transpose) z <- t(z)
   if (missing(width)) width <- min(getOption('width'), ncol(z))
   if (missing(height)) height <- min(getOption('width') * 25 / 80, nrow(z))
 
   if (width != ncol(z) || height != nrow(z)) { # must resample z to specified size
-    z <- Mod(fft(fft(z)[1:height, 1:width], inverse = T))
+    z <- Mod(fft(fft(z)[1:height, 1:width], inverse = TRUE))
   }
 
   if (match.arg(yaxis) == 'up') z <- z[height:1,]
 
-  indices <- (z - min(z, na.rm = T))/diff(range(z, na.rm = T)) # \in [0;1]
+  indices <- (z - min(z, na.rm = TRUE))/diff(range(z, na.rm = TRUE)) # \in [0;1]
   indices <- 1 + indices * (length(alphabet) - 1) # \in [1; length(alphabet)]
 
   if (na.char %in% alphabet && any(is.na(indices)))
